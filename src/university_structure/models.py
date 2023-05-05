9b91436cbd1af78a93194df953aa4757d801eed2
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-06 00:33:15 +0500

    add constraints for models

--- src/university_structure/models.py
+++ src/university_structure/models.py
@@ -1,35 +1,42 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, UniqueConstraint
 from sqlalchemy.orm import relationship
 
-from src.database import Base
+from src.base import Base
 
 
 class Faculty(Base):
     __tablename__ = "faculties"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, unique=True, nullable=False)
     specializations = relationship("Specialization")
+
+
+specialization_profession = Table(
+    "specialization_profession",
+    Base.metadata,
+    Column("specialization_id", ForeignKey("specializations.id"), nullable=False),
+    Column("professions_id", ForeignKey("professions.id"), nullable=False),
+    UniqueConstraint("specialization_id", "professions_id")
+)
 
 
 class Specialization(Base):
     __tablename__ = "specializations"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    faculty_id = Column(Integer, ForeignKey("faculties.id"))
-
-
-specialization_profession = Table(
-    "specialization_profession",
-    Base.metadata,
-    Column("specialization_id", ForeignKey("specializations.id")),
-    Column("professions_id", ForeignKey("professions.id")),
-)
+    name = Column(String, unique=True, nullable=False)
+    faculty_id = Column(Integer, ForeignKey("faculties.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    professions = relationship(
+        "Profession", secondary=specialization_profession, back_populates="specializations"
+    )
 
 
 class Profession(Base):
     __tablename__ = "professions"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, unique=True, nullable=False)
+    specializations = relationship(
+        "Specialization", secondary=specialization_profession, back_populates="parents"
+    )
