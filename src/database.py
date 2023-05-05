87e4210dea0bff73f8d1dd53b4fa56bd96be106d
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-06 00:25:05 +0500

    resolve circural dependency

--- src/database.py
+++ src/database.py
@@ -1,12 +1,8 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 
-
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
-Base = declarative_base()
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
