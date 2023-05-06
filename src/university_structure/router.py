7797a3bbf7a6f15ea2455049a7477dc21de63055
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-06 16:36:17 +0500

    add service and enpoints for Specialization

--- src/university_structure/router.py
+++ src/university_structure/router.py
@@ -1,9 +1,9 @@
-from fastapi import Depends, Body
+from fastapi import Depends, Body, Query
 from fastapi.routing import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.university_structure.services import FacultyService
-from src.university_structure.schemas import OutFaculty, InFaculty
+from src.university_structure.services import FacultyService, SpecializationService
+from src.university_structure.schemas import OutFaculty, InFaculty, OutSpecialization, InSpecialization
 from src.database import get_async_session
 
 router = APIRouter(prefix="/structure")
@@ -35,6 +35,40 @@ async def create_faculty(faculty_data: InFaculty = Body(), session: AsyncSession
         await FacultyService.create_faculty(session, faculty_data)
         return {
             "message": "Факультет успешно создан!"
+        }
+    except Exception as ex:
+        print(ex)
+        return {
+            "message": "Что-то пошло не так!"
+        }
+
+
+@router.get(
+    "/specializations",
+    response_model=list[OutSpecialization],
+    description="Возвращает направления по факультету",
+    tags=["Направление"]
+)
+async def get_specializations(faculty_id: int = Query(), session: AsyncSession = Depends(get_async_session)):
+    try:
+        return await SpecializationService.get_specializations_by_faculty(session, faculty_id)
+    except Exception as ex:
+        print(ex)
+        return {
+            "message": "Что-то пошло не так!"
+        }
+
+
+@router.post(
+    "/specializations/create",
+    description="Создает новое направление",
+    tags=["Направление"]
+)
+async def create_specialization(specialization_data: InSpecialization = Body(), session: AsyncSession = Depends(get_async_session)):
+    try:
+        await SpecializationService.create_specialization(session, specialization_data)
+        return {
+            "message": "Направление успешно создано!"
         }
     except Exception as ex:
         print(ex)
--- src/university_structure/services.py
+++ src/university_structure/services.py
@@ -1,6 +1,6 @@
 from abc import ABC
-from src.university_structure.schemas import InFaculty, OutFaculty
-from src.university_structure.models import Faculty
+from src.university_structure.schemas import InFaculty, OutFaculty, InSpecialization, OutSpecialization
+from src.university_structure.models import Faculty, Specialization
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -36,15 +36,17 @@ class FacultyService(EntityService):
 
 
 class SpecializationService(EntityService):
-    __MODEL_CLASS = Faculty
-    __IN_SCHEMA_CLASS = InFaculty
-    __OUT_SCHEMA_CLASS = OutFaculty
+    __MODEL_CLASS = Specialization
+    __IN_SCHEMA_CLASS = InSpecialization
+    __OUT_SCHEMA_CLASS = OutSpecialization
 
     @classmethod
-    async def get_faculties(cls, session: AsyncSession) -> list[OutFaculty]:
-        rows = await super()._get_list(session, cls.__MODEL_CLASS)
+    async def get_specializations_by_faculty(cls, session: AsyncSession, faculty_id: int) -> list[OutSpecialization]:
+        query = select(cls.__MODEL_CLASS).where(cls.__MODEL_CLASS.faculty_id == faculty_id)
+        cursor = await session.execute(query)
+        rows = [row[0] for row in cursor.all()]
         return [cls.__OUT_SCHEMA_CLASS(id=row.id, name=row.name) for row in rows]
 
     @classmethod
-    async def create_faculty(cls, session: AsyncSession, faculty: InFaculty) -> None:
-        await super()._create(session, cls.__MODEL_CLASS, faculty.dict())
+    async def create_specialization(cls, session: AsyncSession, specialization: InFaculty) -> None:
+        await super()._create(session, cls.__MODEL_CLASS, specialization.dict())
