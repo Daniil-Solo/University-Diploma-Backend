395ffd7dc175ef3b4046ab9b1ceeea5496941b26
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-07 23:30:40 +0500

    add cors for frontend

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 
 from src.university_structure.router import router as university_structure_router
@@ -6,6 +7,18 @@ from src.university_structure.router import router as university_structure_route
 app = FastAPI(title="ПГНИУ: Цифровой помощник", )
 
 app.include_router(university_structure_router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["Content-Type", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"]
+)
 
 if __name__ == "__main__":
     run("app:app", host="127.0.0.1", port=5001, reload=True)
